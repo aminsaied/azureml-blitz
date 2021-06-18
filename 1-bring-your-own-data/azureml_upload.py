4a9c8a63895c86337e9c1ae6039295e266c882b0
amin <amin.saied@gmail.com>
2021-06-18 09:18:34 -0700

    blitz demo

--- 1-bring-your-own-data/azureml_upload.py
+++ 1-bring-your-own-data/azureml_upload.py
@@ -1,10 +1,6 @@
 from azureml.core import Workspace
 
 ws = Workspace.from_config()
-ds = ws.get_default_datastore()
 
-ds.upload_files(
-    ["./wisdom.txt"],
-    target_path="azureml-blitz",
-    overwrite=True,
-)
+# connect to default datastore
+# upload data
